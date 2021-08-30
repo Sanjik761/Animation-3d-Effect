6b429d9ee9a2047e46a0e85d2b8a5f0a57d0f9af
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-30 11:13:29 +0500

    created mousemove function

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const sneaker = document.querySelector('.sneaker img')
 const purchase = document.querySelector('.purchase button')
 const description = document.querySelector('.info h3')
 const sizes = document.querySelector('.sizes')
+
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 10
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 10
+    card.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`
+})
+  
