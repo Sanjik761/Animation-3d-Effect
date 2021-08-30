b329f55c08a0db49e03649535b98c5567ec60dd5
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-30 11:17:09 +0500

    corrected js

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ const description = document.querySelector('.info h3')
 const sizes = document.querySelector('.sizes')
 
 container.addEventListener('mousemove', (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 10
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 10
-    card.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 10
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 10
+  card.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`
 })
 
 container.addEventListener('mouseenter', (e) => {
-    card.style.transition = 'none'
-    title.style.transform = "translateZ(150px)"
-    sneaker.style.transform = "translateZ(200px) rotateZ(1deg)"
-    description.style.transform = "translateZ(125px)"
-    sizes.style.transform = "translateZ(100px)"
-    purchase.style.transform = "translateZ(75px)"
+  card.style.transition = 'none'
+  title.style.transform = "translateZ(150px)"
+  sneaker.style.transform = "translateZ(200px) rotateZ(1deg)"
+  description.style.transform = "translateZ(125px)"
+  sizes.style.transform = "translateZ(100px)"
+  purchase.style.transform = "translateZ(75px)"
 })
-  
+
 container.addEventListener('mouseleave', (e) => {
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`
-    title.style.transform = "translateZ(0px)"
-    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)"
-    description.style.transform = "translateZ(0px)"
-    sizes.style.transform = "translateZ(0px)"
-    purchase.style.transform = "translateZ(0px)"
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`
+  title.style.transform = "translateZ(0px)"
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)"
+  description.style.transform = "translateZ(0px)"
+  sizes.style.transform = "translateZ(0px)"
+  purchase.style.transform = "translateZ(0px)"
 })
