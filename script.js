c5e09e19fc638d07d8974fd636ffd74176668bde
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-30 11:07:44 +0500

    connected classes

---
+++ script.js
@@ -0,0 +1,7 @@
+const card = document.querySelector('.card')
+const container = document.querySelector('.container')
+const title = document.querySelector('.title')
+const sneaker = document.querySelector('.sneaker img')
+const purchase = document.querySelector('.purchase button')
+const description = document.querySelector('.info h3')
+const sizes = document.querySelector('.sizes')
